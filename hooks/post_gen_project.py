750f709fd2bc0bc78456b4fbd8c63dbbdc8a1b7b
Max Hess <44779484+MaksHess@users.noreply.github.com>
2025-06-19 15:05:45 +0200

    Update post_gen_project.py

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -264,7 +264,8 @@ TERMINATOR = "\x1b[0m"
 
 def main() -> None:
     import sys
-    logger.addHandler(logging.StreamHandler(sys.stdout)).setLevel(logging.DEBUG)
+    logger.addHandler(logging.StreamHandler(sys.stdout))
+    logger.setLevel(logging.DEBUG)
     """Run the post generation hooks."""
     set_python_version()
     set_license("{{cookiecutter.license}}")
